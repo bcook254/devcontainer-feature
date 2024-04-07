4a1f468d692329e3dc4f541fe1bd2e829c543377
Benjamin Cook <bcook254@abcook.me>
2024-04-07 09:06:46 -0600

    Initial commit

---
+++ src/podman/install.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/env bash
+
+PODMAN_VERSION=${VERSION}
+
+# Bring in ID, ID_LIKE, VERSION_ID, VERSION_CODENAME
+. /etc/os-release
+# Get an adjusted ID independent of distro variants
+if [ "${ID}" = "debian" ] || [ "${ID_LIKE}" = "debian" ]; then
+    ADJUSTED_ID="debian"
+elif [[ "${ID}" = "rhel" || "${ID}" = "fedora" || "${ID}" = "mariner" || "${ID_LIKE}" = *"rhel"* || "${ID_LIKE}" = *"fedora"* || "${ID_LIKE}" = *"mariner"* ]]; then
+    ADJUSTED_ID="rhel"
+    VERSION_CODENAME="${ID}{$VERSION_ID}"
+else
+    echo "Linux distro ${ID} not supported."
+    exit 1
+fi
+
+if type apt-get > /dev/null 2>&1; then
+    INSTALL_CMD=apt-get
+elif type microdnf > /dev/null 2>&1; then
+    INSTALL_CMD=microdnf
+elif type dnf > /dev/null 2>&1; then
+    INSTALL_CMD=dnf
+elif type yum > /dev/null 2>&1; then
+    INSTALL_CMD=yum
+else
+    echo "(Error) Unable to find a supported package manager."
+    exit 1
+fi
+
+clean_up() {
+    case $ADJUSTED_ID in
+        debian)
+            rm -rf /var/lib/apt/lists/*
+            ;;
+        rhel)
+            rm -rf /var/cache/dnf/*
+            rm -rf /var/cache/yum/*
+            ;;
+    esac
+}
+clean_up
+
+pkg_mgr_update() {
+    if [ ${INSTALL_CMD} = "apt-get" ]; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt-get update..."
+            ${INSTALL_CMD} update -y
+        fi
+    elif [ ${INSTALL_CMD} = "dnf" ] || [ ${INSTALL_CMD} = "yum" ]; then
+        if [ "$(find /var/cache/${INSTALL_CMD}/* | wc -l)" = "0" ]; then
+            echo "Running ${INSTALL_CMD} check-update ..."
+            ${INSTALL_CMD} check-update
+        fi
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if [ ${INSTALL_CMD} = "apt-get" ]; then
+        if ! dpkg -s "$@" > /dev/null 2>&1; then
+            pkg_mgr_update
+            ${INSTALL_CMD} -y install --no-install-recommends "$@"
+        fi
+    elif [ ${INSTALL_CMD} = "dnf" ] || [ ${INSTALL_CMD} = "yum" ]; then
+        _num_pkgs=$(echo "$@" | tr ' ' \\012 | wc -l)
+        _num_installed=$(${INSTALL_CMD} -C list installed "$@" | sed '1,/^Installed/d' | wc -l)
+        if [ ${_num_pkgs} != ${_num_installed} ]; then
+            pkg_mgr_update
+            ${INSTALL_CMD} -y install "$@"
+        fi
+    elif [ ${INSTALL_CMD} = "microdnf" ]; then
+        ${INSTALL_CMD} -y install \
+            --refresh \
+            --best \
+            --nodocs \
+            --noplugins \
+            --setopt=install_weak_deps=0 \
+            "$@"
+    else
+        echo "Linux distro ${ID} not supported."
+        exit 1
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+if type podman > /dev/null 2>&1; then
+    echo "Detected existing system install: $(podman version)"
+    # Clean up
+    clean_up
+    exit 0
+fi
+
+if [ "$INSTALL_CMD" = "apt-get" ]; then
+    echo "Installing podman from OS apt repository"
+else
+    echo "Installing podman from OS yum/dnf repository"
+fi
+if [ $ID = "mariner" ]; then
+    check_packages ca-certificates
+fi
+PACKAGE=podman
+if [ "$PODMAN_VERSION" != "latest"]
+    case "$INSTALL_CMD" in
+        "apt-get")
+            PACKAGE="${PACKAGE}=${PODMAN_VERSION}"
+            ;;
+        *)
+            PACKAGE="${PACKAGE}-${PODMAN_VERSION}"
+    esac
+fi
+check_packages $PACKAGE
+# Clean up
+clean_up
